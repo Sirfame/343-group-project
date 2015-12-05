88cdb9aca263213a1c34cf81e021928b195a0b0a
Joshua Mielke <joshuamielke@gmail.com>
2015-12-05 13:51:55 -0800

    Added basic title page, added angular partials

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@
 window.onload = function() {
 
 };
+
+angular.module('VocabApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
+.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state('home', {
+			url: '/', //"root" directory
+			templateUrl: 'partials/home.html',
+			controller: 'SignupCtrl'
+		})
+})
+
+// Controls the homepage and login/signup screens
+.controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
+	
+}])
