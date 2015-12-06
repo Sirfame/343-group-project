eda50b3d58ed59d029a36ccdfcbba59c9468396e
Joshua Mielke <joshuamielke@gmail.com>
2015-12-05 17:13:25 -0800

    added img folder, added signIn, signUp, dashboard partials, closed #3

--- js/app.js
+++ js/app.js
@@ -11,11 +11,26 @@ angular.module('VocabApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 		.state('home', {
 			url: '/', //"root" directory
 			templateUrl: 'partials/home.html',
-			controller: 'SignupCtrl'
+			controller: 'LoginCtrl'
+		})
+		.state('signIn', {
+			url: '/sign-in',
+			templateUrl: 'partials/signIn.html',
+			controller: 'LoginCtrl'
+		})
+		.state('signUp', {
+			url:'/sign-up',
+			templateUrl: 'partials/signUp.html',
+			controller: 'LoginCtrl'
+		})
+		.state('dashboard', {
+			url: '/dashboard',
+			templateUrl: 'partials/dashboard.html',
+			controller: 'DashboardCtrl'
 		})
 })
 
 // Controls the homepage and login/signup screens
-.controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
 	
 }])
